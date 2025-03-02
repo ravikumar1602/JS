7f929bffc87a5e4bccde679354b9aefffe370455
Ravikumar161 <ravi.rd1619@gmail.com>
2025-03-02 15:47:34 +0530

    learn for loop, while loop, do while loop, conditional statement and create game guess number logic

---
+++ Day_3/conditional.js
@@ -0,0 +1,15 @@
+// let a=5;
+// if(a!=5){
+//     // console.log("a is equal");
+//     console.log("Hello Everyone");
+// }
+// else{
+//     // console.log("a is not equal");
+//     console.log("Hello Student");
+// }
+
+let userInpot;
+do{
+    userInpot=prompt("Enter a word 'Stop' to exit");
+}
+while(userInpot!='stop')
---
+++ Day_3/loops.js
@@ -0,0 +1,20 @@
+// 1    for loop
+// 2    while loop 
+// 3    do while loop
+
+// for (i = 1; i <= 10; i++) {
+//         console.log(i);
+//         console.log();
+//             }
+
+// let i = 0;
+// while (i < 5) {
+//     console.log("Ravi");
+//     i++;
+// }
+
+// let i = 100;
+// do {
+//     console.log(Ravi);
+//     i++;
+// } while (i < 100)
